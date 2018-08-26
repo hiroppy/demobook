a57015c4b0ece21c80d7316a1ccc55304e4fdc84
Yuta Hiroto <git@about-hiroppy.com>
2018-08-26 22:55:35 +0900

    chore: update examples

---
+++ examples/local.sh
@@ -0,0 +1,34 @@
+demobook -o hiroppy -r demobook -t https://localhost:3000 -d output
+demobook -o hiroppy -r demobook -t https://localhost:3000 -d output
+demobook -o hiroppy -r demobook -t https://localhost:3000 -d output2 -n core
+demobook -o hiroppy -r demobook -t https://localhost:3000 -d output2 -n storybook
+
+demobook -o hiroppy -r demo -t https://localhost:3000 -d output
+demobook -o hiroppy -r demo -t https://localhost:3000 -d output2 -n storybook
+
+demobook -o hiroppy -r slides -t https://localhost:3000 -d output
+demobook -o hiroppy -r slides -t https://localhost:3000 -d output2 -n slide
+demobook -o hiroppy -r slides -t https://localhost:3000 -d output
+demobook -o hiroppy -r slides -t https://localhost:3000 -d output2 -n slide
+
+demobook -o hiroppy -r fusuma -t https://localhost:3000 -d output
+demobook -o hiroppy -r fusuma -t https://localhost:3000 -d output
+demobook -o hiroppy -r fusuma -t https://localhost:3000 -d output
+demobook -o hiroppy -r fusuma -t https://localhost:3000 -d output
+demobook -o hiroppy -r fusuma -t https://localhost:3000 -d output
+demobook -o hiroppy -r fusuma -t https://localhost:3000 -d output
+
+demobook -o ah-test -r slides -t https://localhost:3000 -d output
+demobook -o ah-test -r slides -t https://localhost:3000 -d output2 -n slide
+demobook -o ah-test -r slides -t https://localhost:3000 -d output
+demobook -o ah-test -r slides -t https://localhost:3000 -d output2 -n slide
+
+# demobook -o nodejs -r node -t https://localhost:3000 -d output
+# demobook -o nodejs -r node -t https://localhost:3000 -d output2 -n binary
+# demobook -o nodejs -r node -t https://localhost:3000 -d output - n nodejs.org
+# demobook -o nodejs -r node -t https://localhost:3000 -d output2 -n binary
+
+demobook -o nicohaco -r nicohaco -t https://localhost:3000 -d output
+demobook -o nicohaco -r nicohaco -t https://localhost:3000 -d output2
+demobook -o nicohaco -r nicohaco -t https://localhost:3000 -d output
+demobook -o nicohaco -r nicohaco -t https://localhost:3000 -d output2
