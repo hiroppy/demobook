b07b96b7e0229fdb3f54e858aa9ef55df3b374d8
Yuta Hiroto <git@about-hiroppy.com>
2018-08-26 22:55:35 +0900

    chore: update examples

--- examples/pr.sh
+++ examples/pr.sh
@@ -1,2 +1,3 @@
-demobook -o hiroppy -r demobook -t https://localhost:3000 -d output --pr 3
-demobook -o hiroppy -r demobook -t https://localhost:3000 -d output2 -n sub --pr 3
+demobook -o hiroppy -r demobook -t https://localhost:3000 -d output --pr 3 -n storybook
+demobook -o hiroppy -r demobook -t https://localhost:3000 -d output2 -n core --pr 3
+demobook -o hiroppy -r demobook -t https://localhost:3000 -d output2 -n main.bundle.js --pr 3
--- examples/run.sh
+++
@@ -1,2 +0,0 @@
-demobook -o hiroppy -r demobook -t https://localhost:3000 -d output
-demobook -o hiroppy -r demobook -t https://localhost:3000 -d output2 -n sub
